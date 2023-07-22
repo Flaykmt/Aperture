639ee47596c83246fe4dd17f46c48a3d20f77018
louzes <i.mpossible1551@gmail.com>
2023-07-22 17:42:23 +0500

    Create Animations

---
+++ scripts/animate.js
@@ -0,0 +1,53 @@
+ScrollReveal().reveal(".header-anim", {
+  delay: 100,
+  duration: 1500,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  opacity: 0,
+  scale: -2,
+  origin: "bottom",
+  cleanup: true,
+  desktop: true,
+  mobile: true,
+});
+ScrollReveal().reveal(".footer-anim", {
+  delay: 100,
+  duration: 2000,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  opacity: 0,
+  scale: -2,
+  origin: "bottom",
+  cleanup: true,
+  desktop: true,
+  mobile: false,
+});
+ScrollReveal().reveal(".card-anim", {
+  delay: 100,
+  duration: 1500,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  opacity: 0,
+  origin: "bottom",
+  cleanup: true,
+  desktop: true,
+  mobile: true,
+});
+ScrollReveal().reveal(".photo-anim", {
+  delay: 500,
+  duration: 500,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  opacity: 0,
+  origin: "bottom",
+  cleanup: true,
+  desktop: true,
+  mobile: true,
+});
+ScrollReveal().reveal(".photo-anim-bg", {
+  delay: 100,
+  duration: 1500,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  opacity: 0,
+  scale: 2,
+  origin: "bottom",
+  cleanup: true,
+  desktop: true,
+  mobile: false,
+});
